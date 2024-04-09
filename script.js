1d79c767fc72984e67c2da522e1eb2e4d74428ef
analebyodkina <dp030992cad@gmail.com>
2024-04-09 10:19:50 +0300

    ui updates

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function navToggle() {
 	document.getElementById('navUl').classList.toggle("_active");
 	document.getElementById('html').classList.toggle("overflow-y-hidden");
 }
+
+function closeBurger() {
+	document.getElementById('navLi').classList.toggle("_active");
+	document.getElementById('navUl').classList.remove("_active");
+	document.getElementById('html').classList.remove("overflow-y-hidden");
+}
